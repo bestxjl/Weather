c7cdbac476a513a86b8529babe35a2c8c9da2281
bestxjl <317045234@qq.com>
2016-07-08 17:58:09 +0800

    frist commit

---
+++ app/src/main/java/cn/edu/tyu/weather/MainActivity.java
@@ -0,0 +1,17 @@
+package cn.edu.tyu.weather;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zzp on 2016/7/8.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weather);
+    }
+}
