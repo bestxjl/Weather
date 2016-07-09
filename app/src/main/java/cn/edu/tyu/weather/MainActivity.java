0b905a83587f25a2b6730c8d90acc1318ea5b5c5
wuwenchun <496194722@qq.com>
2016-07-09 14:55:11 +0800

    second

--- app/src/main/java/cn/edu/tyu/weather/MainActivity.java
+++ app/src/main/java/cn/edu/tyu/weather/MainActivity.java
@@ -3,6 +3,10 @@ package cn.edu.tyu.weather;
 import android.app.Activity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import cn.edu.tyu.util.NetUtil;
 
 /**
  * Created by zzp on 2016/7/8.
@@ -13,5 +17,13 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.weather);
+        if (NetUtil.getNetworkState(this) != NetUtil.NETWORN_NONE) {
+            Log.d("Weather", "网络OK");
+            Toast.makeText(MainActivity.this,"网络OK！", Toast.LENGTH_LONG).show();
+        }else
+        {
+            Log.d("Weather", "网络挂了");
+            Toast.makeText(MainActivity.this,"网络挂了！", Toast.LENGTH_LONG).show();
+        }
     }
 }
